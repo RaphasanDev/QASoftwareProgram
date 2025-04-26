27e8e4278d68babbf7f15a3aa35660b2d07d6bf8
Raphael San <=>
2025-04-26 17:06:54 -0300

    adding new classes and translating the objects language to pt-br to meet the API Rest

--- littleStoreAPIAutomation/src/test/java/modules/product/ProductTest.java
+++ littleStoreAPIAutomation/src/test/java/modules/product/ProductTest.java
@@ -1,15 +1,20 @@
 package modules.product;
 
 
-import io.restassured.RestAssured;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import pojo.Component;
+import pojo.Product;
+import pojo.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 @DisplayName("Product modules API Rest tests")
@@ -17,19 +22,24 @@ public class ProductTest {
     private String token;
 
     @BeforeEach
-    public void beforeEach(){
+    public void beforeEach() throws JsonProcessingException {
         //Set the data for the littleStore REST API
         baseURI = "http://165.227.93.41";
         //port = "";
         basePath = "/lojinha";
 
+        User user = new User();
+        user.setUsuarioLogin("raphaelsan");
+        user.setUsuarioSenha("123456");
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(user);
+        System.out.println("JSON Gerado: " + json);
+
         //Retrieve the token for the admin user
         this.token = given()
                 .contentType(ContentType.JSON)
-                .body("{\n" +
-                        "  \"usuarioLogin\": \"raphaelsan\",\n" +
-                        "  \"usuarioSenha\": \"123456\"\n" +
-                        "}")
+                .body(user)
                 .when()
                 .post("/v2/login")
                 .then()
@@ -40,31 +50,42 @@ public class ProductTest {
 
     @Test
     @DisplayName("Validate product value 0.00 is not allowed")
-    public void testValidatelimitedLowerProductValues() {
+    public void testValidatelimitedLowerProductValues() throws JsonProcessingException {
         //Attempt to insert a product with a value of 0.00 and validate that an error message is displayed along with status code 422.
+
+        Product product = new Product();
+        product.setProdutoNome("Xbox S Series");
+        System.out.println("Product Name: " + product.getProdutoNome());
+
+        product.setProdutoValor(0.00);
+        List<String> colors = new ArrayList<>();
+        colors.add("preto");
+        colors.add("branco");
+        product.setProdutoCores(colors);
+        product.setProdutoUrlMock("");
+
+        List<Component> components = new ArrayList<>();
+
+        Component component = new Component();
+
+        component.setComponenteNome("Controle");
+        component.setComponenteQuantidade(2);
+        component.setComponenteNome("FC 2024");
+        component.setComponenteQuantidade(1);
+        components.add(component);
+        product.setComponentes(components);
+
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(product);
+        System.out.println("JSON Gerado: " + json);
+
         given()
                 .contentType(ContentType.JSON)
                 .header("token", this.token)
-                .body("{\n" +
-                        "  \"produtoNome\": \"Xbox S Series\",\n" +
-                        "  \"produtoValor\": 0.00,\n" +
-                        "  \"produtoCores\": [\n" +
-                        "    \"preto\"\n" +
-                        "  ],\n" +
-                        "  \"produtoUrlMock\": \"**\",\n" +
-                        "  \"componentes\": [\n" +
-                        "    {\n" +
-                        "      \"componenteNome\": \"Controle\",\n" +
-                        "      \"componenteQuantidade\": 2\n" +
-                        "    }, \n" +
-                        "    {\n" +
-                        "      \"componenteNome\": \"FC 2024\",\n" +
-                        "      \"componenteQuantidade\": 1\n" +
-                        "    }\n" +
-                        "  ]\n" +
-                        "}")
+                .body(product)
                 .when()
-                    .post("/v2/produtos")
+                .post("/v2/produtos")
                 .then()
                 .assertThat()
                 .body("error", equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00"))
